771dd86bf874e2ac36d190df6b9521b3c6b1d9dd
100rabh2812 <149459770+100rabh2812@users.noreply.github.com>
2023-10-31 16:39:24 +0000

    VARIABLE in js

---
+++ 01_Basic/01_variable.js
@@ -0,0 +1,26 @@
+const accountID = 123;
+var accountName = "Saurabh"
+let accountEmail = "saurabh@gmail.com";
+accountCity = "Amravati"
+let accountState ;
+
+// accountID = 5695;
+// console.log(accountID);
+//Error :  Assignment to constant variable.
+
+accountName = "Samesh";
+// console.log(accountName);
+
+accountEmail = "ramesh@gmail.com";
+// console.log(accountEmail);
+
+accountCity = "Nagpur";
+// console.log(accountCity);
+
+/*
+prefer not used var because of scope block and functional scope
+*/
+
+console.table([accountID,accountName,accountEmail,accountCity,accountState]);
+
+
