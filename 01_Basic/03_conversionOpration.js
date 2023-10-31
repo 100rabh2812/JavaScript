23d2396432cbcc01d4e25a2336c7cefb79487000
100rabh2812 <149459770+100rabh2812@users.noreply.github.com>
2023-10-31 17:49:31 +0000

    Opration in js

--- 01_Basic/03_conversionOpration.js
+++ 01_Basic/03_conversionOpration.js
@@ -3,21 +3,21 @@ let score1 = null;
 let score2 = undefined
 let score3 = "saurabh"
 
-console.log(typeof score);
+// console.log(typeof score);
 
 let valueConv = Number(score);
 let valueConv1 = Number(score1);
 let valueConv2 = Number(score2);
 let valueConv3 = Number(score3);
 
-console.log(typeof valueConv);
+// console.log(typeof valueConv);
 
-console.log(typeof NaN);  //NaN  Data type is Number
+// console.log(typeof NaN);  //NaN  Data type is Number
 
-console.log(valueConv1);  // null value return 0
+// console.log(valueConv1);  // null value return 0
 
-console.log(valueConv2);  //undefined return NaN
-console.log(valueConv2);  //string convert string then return NaN
+// console.log(valueConv2);  //undefined return NaN
+// console.log(valueConv2);  //string convert string then return NaN
 
 
 // "33abc" =->NaN
@@ -27,8 +27,16 @@ let isloged = 0;
 
 let loged = Boolean(isloged);
 
-console.log(loged);
+// console.log(loged);
 
 // 1 => true ; 0 => false
 // "" => false
 // "saurabh" = true
+
+// ^^^^^^^^^^^********************^^^^^^^^^^^^ Oprations ^^^^^^^^^^^^^****************^^^^^^^^^^^^
+
+
+console.log(2 + "2");
+console.log("2" + 2);
+// console.log("2" + 2 + 2);  //start in string concat all number
+console.log(2 + 2 + "2");  //start in number addition and string concat
