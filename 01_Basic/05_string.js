6f6855aa0a911cf05e169fc2bafdf32691507c62
100rabh2812 <149459770+100rabh2812@users.noreply.github.com>
2023-11-02 17:46:22 +0000

    String JS

---
+++ 01_Basic/05_string.js
@@ -0,0 +1,36 @@
+let name = "saurabh";
+
+let age = 23;
+
+console.log("my name is " + name + " and age is" +age );
+console.log(`my name is ${name} and age is ${age} `);  // the method is StringInterPolational
+
+
+const gameName = new String('hitesh-hc-com')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = "   hitesh    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('sundar'))
+
+console.log(gameName.split('-'));
